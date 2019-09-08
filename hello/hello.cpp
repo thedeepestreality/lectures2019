e65a4a4fdd4edb566c4303be0e7286cbf24db4fb
thedeepestreality <zetoster@gmail.com>
2019-09-08 21:35:50 +0300

    07.09.2019: Hello world

---
+++ hello/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//using namespace std;
+
+/*
+using std::cout;
+using std::endl;
+*/
+
+int main()
+{
+	std::cout << "Hello!" << std::endl;
+	int x;
+	std::cin >> x;
+	std::cout << x << std::endl;
+
+	return 0;
+}
