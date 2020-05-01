f1264a27617445ec0e0163ff80c17df9031178f6
thedeepestreality <zetoster@gmail.com>
2020-05-02 01:49:15 +0300

    refactored binary operators

--- SuperCalculator/Source.cpp
+++ SuperCalculator/Source.cpp
@@ -1,82 +1,49 @@
 #include <iostream>
 #include <stack>
 #include "formula_node.h"
+#include <functional>
+#include <map>
 
-int calc_postfix(const std::string& formula_str)
-{
+std::map<char, std::function<double(double, double)>> function_map = {
+	{'+', std::plus<double>()},
+	{'-', std::minus<double>()},
+	{'*', std::multiplies<double>()},
+	{'/', std::divides<double>()}
+};
+
+int calc_postfix(const std::string& formula_str){
 	std::stack<int> container;
-	for (char curr : formula_str)
-		switch (curr) {
-			case '+':{
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left + right);
-				break;
-			}
-			case '-': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left - right);
-				break;
-			}
-			case '*': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left * right);
-				break;
-			}
-			case '/': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left / right);
-				break;
-			}
-			default:
-				if (isdigit(curr))
-					container.push(curr - '0');
+	for (char curr : formula_str){
+		if (isdigit(curr)){
+			container.push(curr - '0');
+			continue;
 		}
+		char right = container.top(); container.pop();
+		char left = container.top(); container.pop();
+		container.push(function_map[curr](left,right));
+	}
+
 	return container.top();
 }
 
 class Formula {
 	FormulaNode* _root;
+	std::map<char, std::function<FormulaNode*(FormulaNode*, FormulaNode*)>> function_map = {
+		{'+', [](FormulaNode* l, FormulaNode* r) {return new PlusNode(l,r); }},
+		{'-', [](FormulaNode* l, FormulaNode* r) {return new MinusNode(l,r); }},
+		{'*', [](FormulaNode* l, FormulaNode* r) {return new MultNode(l,r); }},
+		{'/', [](FormulaNode* l, FormulaNode* r) {return new DivNode(l,r); }}
+	};
 	FormulaNode* from_postfix(const std::string& postfix_str) {
 		std::stack<FormulaNode*> nodes;
 		for (char curr : postfix_str) {
-			FormulaNode* node;
-			switch (curr) {
-				case '+':
-				{
-					auto right = nodes.top(); nodes.pop();
-					auto left = nodes.top(); nodes.pop();
-					node = new PlusNode(left, right);
-					break;
-				}
-				case '-':
-				{
-					auto right = nodes.top(); nodes.pop();
-					auto left = nodes.top(); nodes.pop();
-					node = new MinusNode(left, right);
-					break;
-				}
-				case '*':
-				{
-					auto right = nodes.top(); nodes.pop();
-					auto left = nodes.top(); nodes.pop();
-					node = new MultNode(left, right);
-					break;
-				}
-				case '/':
-				{
-					auto right = nodes.top(); nodes.pop();
-					auto left = nodes.top(); nodes.pop();
-					node = new DivNode(left, right);
-					break;
-				}
-				default:
-					if (isdigit(curr)) node = new NumNode(curr - '0');
-			}
-			nodes.push(node);
+			if (isdigit(curr)){
+				nodes.push(new NumNode(curr - '0'));
+					continue;
+			}	
+			auto right = nodes.top(); nodes.pop();
+			auto left = nodes.top(); nodes.pop();		
+			nodes.push(function_map[curr](left,right));
 		}
 		return nodes.top();
 	}
@@ -97,6 +64,7 @@ int main() {
 	//wrong translation: "23+4*"
 	//"-2" <-> "02-"
 	//std::cout << calc_postfix("23+4*52/-") << std::endl;
+	
 	Formula f("23+4*52/-");
 	try{
 		std::cout << f.str() << " = " << f.calc() << std::endl;
