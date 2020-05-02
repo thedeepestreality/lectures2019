5206a4b0abfafa2dc5d2aa61c6451cf1a2d2907a
thedeepestreality <zetoster@gmail.com>
2020-05-02 11:00:49 +0300

    02.05.2020: postfix calc refactor

--- SuperCalculator/Source.cpp
+++ SuperCalculator/Source.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
 #include <stack>
 #include "formula_node.h"
+#include <map>
+#include <functional>
 
-int calc_postfix(const std::string& formula_str)
+std::map<char, std::function<double(double, double) >> operator_map =
 {
-	std::stack<int> container;
-	for (char curr : formula_str)
-		switch (curr) {
-			case '+':{
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left + right);
-				break;
-			}
-			case '-': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left - right);
-				break;
-			}
-			case '*': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left * right);
-				break;
-			}
-			case '/': {
-				char right = container.top(); container.pop();
-				char left = container.top(); container.pop();
-				container.push(left / right);
-				break;
-			}
-			default:
-				if (isdigit(curr))
-					container.push(curr - '0');
+	{'+', std::plus<double>()},
+	{'-', std::minus<double>()},
+	{'*', std::multiplies<double>()},
+	{'/', std::divides<double>()}
+};
+
+double calc_postfix(const std::string& formula_str){
+	std::stack<double> container;
+	for (char curr : formula_str){
+		if (isdigit(curr)) {
+			container.push(curr - '0');
+			continue;
 		}
+		double right = container.top(); container.pop();
+		double left = container.top(); container.pop();
+		container.push(operator_map[curr](left,right));
+	}
 	return container.top();
 }
 
@@ -91,18 +79,23 @@ public:
 };
 
 int main() {
+	setlocale(LC_ALL, "Rus");
 	//"2+2" <-> "22+"
 	//"4*(2+3)-5/2" <-> "23+4*52/-"
 	//"2+(3*4)" <-> "234*+" <-> "34*2+"
 	//wrong translation: "23+4*"
 	//"-2" <-> "02-"
-	//std::cout << calc_postfix("23+4*52/-") << std::endl;
-	Formula f("23+4*52/-");
+
+	std::cout << calc_postfix("23+4*50/-") << std::endl;
+	//double x = 0;
+	//std::cout << 1. / x << std::endl;
+	/*Formula f("23+4*52/-");
 	try{
 		std::cout << f.str() << " = " << f.calc() << std::endl;
 	} catch (const Error& e){
 		std::cout << "Error: " << e.what() << std::endl;
-	}
+	}*/
+
 	system("pause");
 	return 0;
 }
